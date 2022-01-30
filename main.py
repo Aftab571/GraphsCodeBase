64bdb504cc3f61b014cb8bcee719dca4d484c058
aftab <mohd.aftab571@gmail.com>
2022-01-30 20:05:23 +0100

    Added requirements file to repo

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ def main():
     neoData_node= neoConn.fetch_data(node_query)
     data, stats= prep.DataPrep.convertData(df1=neoData_edge,df2= neoData_node)
     print(stats)
+    file = open("stats.txt","r+")
+    file.truncate(0)
+    file.close()
+    with open('stats.txt', 'w') as f:
+        f.write(str(stats)+"\n")
+        f.close()
     for i in range(10):
         gcn_stats,gcn_loss = gcn.callGCN(data)
         gat_stats,gat_loss = gat.callGAT(data)
@@ -36,11 +42,31 @@ def main():
         gat_list.append(gat_stats['test_acc'])
         gsage_list.append(gsage_stats['test_acc'])
         iter_list.append(i)
+        with open('stats.txt', 'a') as f:
+            iteration = "----------------Iteration"+str(i)+"----------------------\n"
+            gcn_stats_str = "GCN Stats:"+str(gcn_stats)+"\n"
+            gat_stats_str = "GAT Stats:"+str(gat_stats)+"\n"
+            gsage_stats_str = "GSage Stats:"+str(gsage_stats)+"\n"
+            
+            f.write(iteration)
+            f.write(gcn_stats_str)
+            f.write(gat_stats_str)
+            f.write(gsage_stats_str)
+            f.close()
         print("----------------Iteration",str(i),"----------------------")
         print("GCN Stats:",str(gcn_stats))
         print("GAT Stats:",str(gat_stats))
         print("GSage Stats:",str(gsage_stats))
         vis.plot_train_loss(gcn_loss,gat_loss,i)
+        
+    with open('stats.txt', 'a') as f:
+        avg_gcn= "Average Test Accuracy for GCN: "+str(Average(gcn_list))+"\n"
+        avg_gat= "Average Test Accuracy for GAT: "+str(Average(gat_list))+"\n"
+        avg_gsage= "Average Test Accuracy for GSAGE: "+str(Average(gsage_list))+"\n"
+        f.write(avg_gcn)
+        f.write(avg_gat)
+        f.write(avg_gsage)
+        f.close()
     
     print("Average Test Accuracy for GCN: ", str(Average(gcn_list)))
     print("Average Test Accuracy for GAT: ", str(Average(gat_list)))
